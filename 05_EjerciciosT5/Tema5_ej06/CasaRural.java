5c2e47aadc1d00ef164618bcf906fd926ca671ae
Romeo <romejp451@gmail.com>
2023-03-14 19:08:26 +0100

    Add files via upload

---
+++ 05_EjerciciosT5/Tema5_ej06/CasaRural.java
@@ -0,0 +1,13 @@
+package tema5_ej06;
+
+/**
+ *
+ * @author Romeo <romejp451@gmail.com>
+ */
+public class CasaRural extends Hotel{
+    
+    public CasaRural(String d) {
+        super(d, 3, 6);
+    }
+    
+}
