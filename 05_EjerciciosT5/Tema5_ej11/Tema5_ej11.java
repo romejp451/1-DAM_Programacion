a7c36e82d73aa0657773eb00b7ab4b0f6ce1bccb
Romeo <romejp451@gmail.com>
2023-03-19 18:21:57 +0100

    Add files via upload

---
+++ 05_EjerciciosT5/Tema5_ej11/Tema5_ej11.java
@@ -0,0 +1,13 @@
+package tema5_ej11;
+import tema5_ej11.Empleado;
+/**
+ *
+ * @author Romeo <romejp451@gmail.com>
+ */
+public class Tema5_ej11 {
+    public static void main(String[] args) {
+        Empleado m1= new Empleado("Pepe", 1000);
+        Empleado m2= new CuerpoTecnico("Manuel", 2000, "Tecnico");
+    }
+    
+}
