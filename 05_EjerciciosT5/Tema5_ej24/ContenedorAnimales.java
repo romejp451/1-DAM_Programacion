694fc89e89274126a7bd218524a0180dadb652a9
Romeo <romejp451@gmail.com>
2023-04-18 17:30:14 +0200

    Add files via upload

---
+++ 05_EjerciciosT5/Tema5_ej24/ContenedorAnimales.java
@@ -0,0 +1,51 @@
+package daw.zoo;
+import  daw.zoo.Animal;
+import java.util.*;
+
+/**
+ *
+ * @author Romeo <romejp451@gmail.com>
+ */
+public abstract class ContenedorAnimales {
+    //VALORES
+    protected Set <Animal> animales;
+    
+    //CONSTRUCTOR
+
+    public ContenedorAnimales() {
+        List<Animal> animales = new ArrayList<Animal>(this.animales);
+    };
+    
+    public boolean añadir(Animal a){
+        if (this.comprobarDisponibilidadPlaza(a) == true) {
+            this.animales.add(a);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public boolean retirar(Animal a){
+        if (this.animales.contains(a)) {
+            this.animales.remove(a);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public int getPesoActual(){
+        int aux = 0;
+        for (Animal animal : animales) {
+            int peso = animal.getPeso();
+            aux += peso;
+        }
+        return aux;
+    }
+    
+    public int getNumeroAnimales(){
+        return animales.size();
+    }
+    
+    protected abstract boolean comprobarDisponibilidadPlaza(Animal a);
+}
