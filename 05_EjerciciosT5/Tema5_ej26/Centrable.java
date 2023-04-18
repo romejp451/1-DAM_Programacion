c516058d69b3ce8b239135b28b811c0fcadb525d
Romeo <romejp451@gmail.com>
2023-04-18 17:31:32 +0200

    Add files via upload

---
+++ 05_EjerciciosT5/Tema5_ej26/Centrable.java
@@ -0,0 +1,11 @@
+package tema5_ej26;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author Romeo <romejp451@gmail.com>
+ */
+public interface Centrable {
+    public Point getCentro();
+}
---
+++ 05_EjerciciosT5/Tema5_ej26/Coloreable.java
@@ -0,0 +1,10 @@
+package tema5_ej26;
+import java.awt.Color;
+
+/**
+ *
+ * @author Romeo <romejp451@gmail.com>
+ */
+public interface Coloreable {
+    public Color getColor();
+}
